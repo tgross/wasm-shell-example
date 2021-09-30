f95d3980b7d77a446d1b5c23cf466af33a0792d7
Tim Gross <tim@0x74696d.com>
2021-09-29 23:46:07 -0400

    host side of kv set/get functions
    Read the memory locations passed from the guest as parameters and
    response buffers, and fill the response with the appropriate state.

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::convert::TryInto;
 use std::env;
 use std::io::prelude::*;
 use std::os::unix::net::{UnixListener, UnixStream};
@@ -10,13 +12,23 @@ use wasmtime::*;
 use wasmtime_wasi::sync::WasiCtxBuilder;
 use wasmtime_wasi::{WasiCtx, WasiFile};
 
+// Memory is by default exported from Rust modules under the
+// name `memory`. This can be tweaked with the -Clink-arg flag
+// to rustc to pass flags to LLD, the WebAssembly code linker.
+const MEMORY: &str = &"memory";
+const MAX_PARAMETER_SIZE: u32 = 1024;
+
 struct State {
     counts: Vec<i32>,
+    map: HashMap<String, String>,
 }
 
 impl State {
     fn new() -> State {
-        State { counts: Vec::new() }
+        State {
+            counts: Vec::new(),
+            map: HashMap::new(),
+        }
     }
 
     fn add(&mut self, val: i32) {
@@ -74,7 +86,32 @@ fn main() -> Result<()> {
          res_ptr: u32,
          res_len: u32|
          -> u32 {
-            return 0;
+            let mem = match caller.get_export(MEMORY) {
+                Some(Extern::Memory(mem)) => mem,
+                _ => {
+                    eprintln!("module did not have a memory export");
+                    return 0;
+                }
+            };
+            let store = caller.as_context();
+            let result = kv_set(
+                mem, &store, key_ptr, key_len, val_ptr, val_len, res_ptr, res_len,
+            );
+            let mut store = caller.as_context_mut();
+            match result {
+                Ok(response) => {
+                    return write_response(mem, &mut store, res_ptr, res_len, response)
+                        .map_err(|err| eprintln!("{}", err))
+                        .unwrap_or(0);
+                }
+                Err(err) => {
+                    let response = format!("{}", err);
+                    eprintln!("{}", err);
+                    return write_response(mem, &mut store, res_ptr, res_len, response)
+                        .map_err(|err| eprintln!("{}", err))
+                        .unwrap_or(0);
+                }
+            }
         },
     )?;
 
@@ -87,7 +124,30 @@ fn main() -> Result<()> {
          res_ptr: u32,
          res_len: u32|
          -> u32 {
-            return 0;
+            let mem = match caller.get_export(MEMORY) {
+                Some(Extern::Memory(mem)) => mem,
+                _ => {
+                    eprintln!("module did not have a memory export");
+                    return 0;
+                }
+            };
+            let store = caller.as_context();
+            let result = kv_get(mem, &store, key_ptr, key_len, res_ptr, res_len);
+            let mut store = caller.as_context_mut();
+            match result {
+                Ok(response) => {
+                    return write_response(mem, &mut store, res_ptr, res_len, response)
+                        .map_err(|err| eprintln!("{}", err))
+                        .unwrap_or(0);
+                }
+                Err(err) => {
+                    let response = format!("{}", err);
+                    eprintln!("{}", err);
+                    return write_response(mem, &mut store, res_ptr, res_len, response)
+                        .map_err(|err| eprintln!("{}", err))
+                        .unwrap_or(0);
+                }
+            }
         },
     )?;
 
@@ -173,4 +233,77 @@ fn handle_client(
         }
         return;
     };
+}
+
+fn read_parameter(
+    mem: Memory,
+    store: &StoreContext<StoreData>,
+    ptr: u32,
+    len: u32,
+) -> Result<String> {
+    validate_wasm_param(ptr, len)?;
+    let mut buf = vec![0u8; len as usize];
+    mem.read(&store, ptr.try_into()?, &mut buf)?;
+    Ok(std::str::from_utf8(&buf)?.to_string())
+}
+
+fn write_response(
+    mem: Memory,
+    store: &mut StoreContextMut<StoreData>,
+    ptr: u32,
+    max_len: u32,
+    mut response: String,
+) -> Result<u32> {
+    response.truncate(max_len.try_into()?);
+    mem.write(store, ptr.try_into()?, response.as_bytes())?;
+    Ok(response.len() as u32)
+}
+
+fn validate_wasm_param(ptr: u32, len: u32) -> Result<()> {
+    if ptr == 0 || len == 0 {
+        return Err(anyhow!("pointer and length need must be non-zero"));
+    }
+    if len > MAX_PARAMETER_SIZE {
+        return Err(anyhow!("parameter exceed maximum length"));
+    }
+    Ok(())
+}
+
+fn kv_set(
+    mem: Memory,
+    store: &StoreContext<StoreData>,
+    key_ptr: u32,
+    key_len: u32,
+    val_ptr: u32,
+    val_len: u32,
+    res_ptr: u32,
+    res_len: u32,
+) -> Result<String> {
+    let key = read_parameter(mem, &store, key_ptr, key_len)?;
+    let val = read_parameter(mem, &store, val_ptr, val_len)?;
+    validate_wasm_param(res_ptr, res_len)?;
+    let max_len: usize = res_len.try_into().unwrap_or(1024);
+
+    store.data().state.lock().unwrap().map.insert(key, val);
+    let mut response = "ok".to_string();
+    response.truncate(max_len);
+    Ok(response)
+}
+
+fn kv_get(
+    mem: Memory,
+    store: &StoreContext<StoreData>,
+    key_ptr: u32,
+    key_len: u32,
+    res_ptr: u32,
+    res_len: u32,
+) -> Result<String> {
+    let key = read_parameter(mem, &store, key_ptr, key_len)?;
+    validate_wasm_param(res_ptr, res_len)?;
+    let max_len: usize = res_len.try_into().unwrap_or(1024);
+
+    let map = &store.data().state.lock().unwrap().map;
+    let mut response = map.get(&key).ok_or(anyhow!("no such key"))?.to_string();
+    response.truncate(max_len);
+    Ok(response)
 }
